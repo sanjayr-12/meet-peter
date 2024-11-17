f29f140b780589e213c01fb54476db7c4e5d7a98
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-11-17 19:58:49 +0530

    updated delete button

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -3,18 +3,16 @@ import useStore from "../../store/zustand";
 import { googleLogout } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import "./nav.css";
+import { useState } from "react";
 const Nav = () => {
   const user = useStore((state) => state.user);
   const setToken = useStore((state) => state.setToken);
   const setRender = useStore((state) => state.setRender);
   const naviagte = useNavigate();
+  const [deleteLoading, setDeleteLoading] = useState(false);
   const handleLogout = async () => {
     try {
-      await axios.post(
-        "/api/user/logout",
-        {},
-        { withCredentials: true }
-      );
+      await axios.post("/api/user/logout", {}, { withCredentials: true });
       naviagte("/");
       googleLogout();
       setToken(null);
@@ -36,12 +34,15 @@ const Nav = () => {
     );
     if (command === "DELETE") {
       try {
+        setDeleteLoading(true);
         await axios.delete("/api/chats/delete", {
           withCredentials: true,
         });
         setRender();
       } catch (error) {
         console.log(error);
+      } finally {
+        setDeleteLoading(false);
       }
     } else {
       alert("Wrong Command, make sure to have no spaces");
@@ -50,8 +51,8 @@ const Nav = () => {
   return (
     <div className="nav-main-container">
       <h3>Meet Peter</h3>
-      <button onClick={handleDelete} className="delete">
-        delete
+      <button onClick={handleDelete} className="delete" disabled={deleteLoading}>
+        {deleteLoading?"deleting...":"delete"}
       </button>
       <div className="nav-container">
         <h3>{user ? toSentanceCase(user?.name) : "Loading..."}</h3>
