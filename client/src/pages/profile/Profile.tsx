f90cd271aff672d797b4bcce93409d9a47a45cd3
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-01-09 11:57:18 +0530

    profile page ui updated

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,7 +1,28 @@
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+import useStore from "../../store/zustand";
 
-export default Profile
+const Profile = () => {
+  const user = useStore((state) => state.user);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-10">
+      <h1 className="text-center text-xl">Update Profile</h1>
+      <form className="flex flex-col gap-9">
+        <label className="input input-bordered flex items-center gap-2 p-6">
+          Image Url
+          <input
+            type="text"
+            className="grow"
+            placeholder="https://some.com/chicken.png"
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          Name
+          <input type="text" className="grow" placeholder={user?.name} />
+              </label>
+              <input type="submit" className="btn btn-ghost" value="Update"/>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
